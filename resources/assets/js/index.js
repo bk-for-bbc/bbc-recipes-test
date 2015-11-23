05488a605a0c28f71ffd3dd5214912a77d525184
Ben King <ben@designbybenking.co.uk>
2015-11-23 23:29:25 +0000

    Setup index page

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,6 +1,32 @@
 const angular = require('angular');
 
 angular.module('bbc-recipes', [require('angular-ui-router')])
-    .component('helloWorld', {
-        template: `Hello... it's me.`
+    .component('bbcRecipesIndex', {
+        controller: function($http) {
+            $http.get('/api/recipes').success((data) => {
+                this.recipes = data;
+                console.log('Index', this.recipes);
+            });
+        },
+        template: `
+        <div ng-if="bbcRecipesIndex.recipes !== undefined">
+            <h2>Browse</h2>
+            <div class="warning--empty" ng-if="!bbcRecipesIndex.recipes.length">
+                Sorry, we currently have no recipes for you
+            </div>
+            <div class="recipes--list">
+                <div class="recipe" ng-repeat="recipe in bbcRecipesIndex.recipes">
+                    <a class="recipe--link" href="">
+                        <img class="recipe--image" ng-src="{{ recipe.image }}">
+                        <div class="recipe--body">
+                            <h3>{{ recipe.name }}</h3>
+                            <p class="recipe--time"><i class="fa fa-clock-o"></i> {{ recipe.cooking_time }} minutes</p>
+                            <div>
+                                <span class="recipe--ingredient" ng-repeat="ingredient in recipe.ingredients">{{ ingredient.name }}</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>`
     });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,47 +2,21 @@
 <html ng-app="bbc-recipes">
     <head>
         <title>BBC Recipes</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link rel="stylesheet" href="/css/app.css"></link>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <h1>Hello</h1>
-                <hello-world></hello-world>
+        <header class="header">
+            <div class="container--main">
+                <a class="logo" href="/" target="_self">
+                    <img src="/images/logo.svg">
+                    <span>Recipes</span>
+                </a>
             </div>
+        </header>
+        <div class="container--main">
+            <bbc-recipes-index></bbc-recipes-index>
         </div>
-
         <script type="text/javascript" src="/js/index.js"></script>
     </body>
 </html>
