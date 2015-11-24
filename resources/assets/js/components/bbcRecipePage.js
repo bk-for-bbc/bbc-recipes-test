ccc89ddb9af0574868748c3cdfef8dbdde7e767a
Ben King <ben@designbybenking.co.uk>
2015-11-24 00:13:59 +0000

    Add individual recipe page design

--- resources/assets/js/components/bbcRecipePage.js
+++ resources/assets/js/components/bbcRecipePage.js
@@ -13,8 +13,33 @@ app.component('bbcRecipePage', {
         });
     },
     template: `
+    <div class="backlinks">
+        <a href="/" class="backlink"><i class="fa fa-chevron-left"></i>Back to Recipes</a>
+    </div>
     <div ng-if="bbcRecipePage.found === true">
-        {{ bbcRecipePage.recipe }}
+        <div class="row">
+            <div class="col-sm-3">
+                <img ng-src="{{ bbcRecipePage.recipe.image }}">
+            </div>
+            <div class="col-sm-9">
+                <h3>{{ bbcRecipePage.recipe.name }}</h3>
+                <p class="recipe--time"><i class="fa fa-clock-o"></i> {{ bbcRecipePage.recipe.cooking_time }} minutes</p>
+                <div class="recipe--ingredients-table">
+                    <table class="table">
+                        <thead>
+                            <th>Qty.</th>
+                            <th>Ingredient</th>
+                        </thead>
+                        <tbody>
+                            <tr ng-repeat="ingredient in bbcRecipePage.recipe.ingredients">
+                                <td class="recipe--ingredients-table-qty">{{ ingredient.quantity }}</td>
+                                <td>{{ ingredient.name }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="warning--empty" ng-if="bbcRecipePage.found === false">
         Sorry, this recipe doesn't exist or may have been removed
