95dadb846f85151703893b2dd685844bc51a949c
Ben King <ben@designbybenking.co.uk>
2015-11-24 15:17:21 +0000

    Update tab styling

--- resources/assets/js/components/bbcRecipesIndex.js
+++ resources/assets/js/components/bbcRecipesIndex.js
@@ -3,8 +3,8 @@ const app = require('../app');
 app.component('bbcRecipesIndex', {
     template: `
         <ul class="nav nav-tabs">
-            <li role="presentation" ui-sref-active="active"><a ui-sref="index.browse">Browse</a></li>
-            <li role="presentation" ui-sref-active="active"><a ui-sref="index.starred">Starred</a></li>
+            <li role="presentation" ui-sref-active="active"><a ui-sref="index.browse"><i class="fa fa-book"></i>Browse</a></li>
+            <li role="presentation" ui-sref-active="active"><a ui-sref="index.starred"><i class="fa fa-star"></i>Starred</a></li>
         </ul>
         <div ui-view></div>`
 });
